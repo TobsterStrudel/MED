1c4031bce4c809ed79b7cfb7d2fd06858033b4f6
TobsterStrudel <tchubs15@gmail.com>
2022-05-02 22:43:32 -0400

    initial files fixed and ready for dev

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by tobias on 5/2/22.
+//
+
+#ifndef MED_MAIN_H
+#define MED_MAIN_H
+
+
+#include <iostream>
+
+
+int main(int argc, char *argv[]){
+    std::cout << "test";
+}
+
+
+#endif //MED_MAIN_H
